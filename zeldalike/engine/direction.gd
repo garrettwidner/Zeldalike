f1ea57ac8259a1f19384453c21911f31341c6960
Garrett Widner <garrettrwidner@gmail.com>
2019-05-25 08:18:44 -0700

    Got basic animation and directionality working

---
+++ zeldalike/engine/direction.gd
@@ -0,0 +1,6 @@
+extends Node
+
+const LEFT : Vector2 = Vector2(-1,0)
+const RIGHT : Vector2 = Vector2(1,0)
+const UP : Vector2 = Vector2(0,-1)
+const DOWN : Vector2 = Vector2(0,1)
--- zeldalike/player/player.gd
+++ zeldalike/player/player.gd
@@ -1,10 +1,21 @@
 extends KinematicBody2D
 
-var speed : float = 20
+var speed : float = 33
+var input : Vector2 = Vector2(0,0)
+onready var anim : AnimationPlayer = $anim
+var facingdir : Vector2 = Vector2(0,0)
 
 func _process(delta):
-	var input: Vector2 = Vector2(0,0)
+	set_input()
+	set_facing_dir()
+	animate()
 	
+	
+	move_and_slide(input * speed, Vector2(0,-1))
+	
+	
+	
+func set_input():
 	if Input.is_action_pressed("left"):
 		input.x = -1
 	elif Input.is_action_pressed("right"):
@@ -18,10 +29,30 @@ func _process(delta):
 		input.y = 1
 	else:
 		input.y = 0
-	
-	move_and_slide(input * speed, Vector2(0,-1))
-	
-	
-	
+		
+func set_facing_dir():
+	if input.x == 0 and input.y == 0:
+		return null
+	elif input.x == 1:
+		facingdir = dir.RIGHT
+	elif input.x == -1:
+		facingdir = dir.LEFT
+	elif input.y == 1:
+		facingdir = dir.DOWN
+	elif input.y == -1: 
+		facingdir = dir.UP
+		
+func animate():
+	match facingdir:
+		dir.UP:
+			anim.play("walkup")
+		dir.DOWN:
+			anim.play("walkdown")
+		dir.LEFT:
+			anim.play("walkleft")
+		dir.RIGHT:
+			anim.play("walkright")
+	if input.x == 0 and input.y == 0:
+		anim.stop(true)
 	
 	pass
