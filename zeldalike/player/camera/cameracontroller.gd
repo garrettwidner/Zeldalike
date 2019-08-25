80e2315c6c195a815294e48c6aa698cbf6995af0
Garrett Widner <garrettrwidner@gmail.com>
2019-08-25 15:27:25 -0700

    Removed some chaff from camera script

--- zeldalike/player/camera/cameracontroller.gd
+++ zeldalike/player/camera/cameracontroller.gd
@@ -2,7 +2,6 @@ extends Position2D
 
 onready var cameragridsnapper = get_node("../cameragridsnapper")
 onready var camerapivot = get_node("../pivot")
-#onready var cameraoffset = get_node("../pivot/cameraoffset")
 onready var camera = get_node("../pivot/cameraholder/Camera2D")
 onready var cameraholder = get_node("../pivot/cameraholder")
 
@@ -21,17 +20,14 @@ enum  TYPE {LOCKED,FREE}
 func _ready():
 	grid_size = get_viewport().get_visible_rect().size
 	player = get_parent()
-	switch_camera_style()
+	switch_to_free()
 	
 	grid_quadrant = get_camera_quadrant()
 	cameragridsnapper.set_quadrant(grid_quadrant)
 	
 func _process(delta):
-#	if Input.is_action_just_pressed("a"):
-#		switch_camera_style()
 	grid_quadrant = get_camera_quadrant()
 	cameragridsnapper.set_quadrant(grid_quadrant)
-#	print(camhold)
 		
 func get_camera_quadrant():
 	var quadrant_x = int(player.position.x / grid_size.x)
@@ -43,12 +39,6 @@ func get_camera_quadrant():
 
 	return Vector2(quadrant_x, quadrant_y)
 
-func switch_camera_style():
-	if camstyle == TYPE.FREE:
-		switch_to_locked()
-	else:
-		switch_to_free()
-	
 func switch_to_free():
 	cameragridsnapper.remove_child(camera)
 	cameraholder.add_child(camera)
@@ -64,25 +54,19 @@ func switch_to_locked():
 func _on_Area2D_body_entered(body, obj):
 	if body.get_name() == "player":
 		print(obj.name)
-#		print("Player entered cam area")
 		camhold += 1
 		
 		var new_extents = obj.get_node("CollisionShape2D").shape.extents
 		camerapivot.set_constraints(new_extents,obj.global_position)
 	
 		if camstyle == TYPE.LOCKED:
-#			print("Moved from a locked area to a free area")
-			
 			switch_to_free()
 
 func _on_Area2D_body_exited(body, obj):
 	if body.get_name() == "player":
-#		print("Player exited cam area")
 		if camhold > 1:
-#			print("Moved from one free movement area to another")
 			pass
 		else:
-#			print("Moved from a free movement area to a locked area")
 			if camstyle == TYPE.FREE:
 				switch_to_locked()
 			
