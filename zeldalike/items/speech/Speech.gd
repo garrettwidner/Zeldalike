da6245e8bd199f8c7776b66ac9fe5920588f17a6
Garrett Widner <garrettrwidner@gmail.com>
2020-02-19 11:10:23 -0700

    Added ability to damage enemies if speech is purified. Committing before creating stamina meter

--- zeldalike/items/speech/Speech.gd
+++ zeldalike/items/speech/Speech.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 var TYPE : String = "PLAYER"
-const DAMAGE : int = 0
+var DAMAGE : int = 0
 var maxamount : int = 2
+var is_purified = false
 
 func _ready():
 	TYPE = "PLAYER"
 	$anim.play("sound")
+	purify_speech()
 
-
+func purify_speech():
+	is_purified = true
+	DAMAGE = 2
 
 func _on_anim_animation_finished(anim_name):
 	queue_free()
